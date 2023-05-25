7b3b405cac8aec2b3cf04a21859b468cd749870f
HyeonJin Song <shwj1602@naver.com>
2023-05-26 01:15:07 +0900

    Remove: Test 오류나서 삭제

--- src/main/java/com/webflux/webfluxpractice/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webflux.webfluxpractice.repository;
-
-import com.webflux.webfluxpractice.entity.Employee;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends ReactiveMongoRepository<Employee, String> {
-}
